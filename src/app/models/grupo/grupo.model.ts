c0c9c65d74a586dbf0bfed4473cc80c947f9e1b6
ghstll <129940439+ghstll@users.noreply.github.com>
2025-11-26 20:17:07 -0600

    grupo : model y service

---
+++ src/app/models/grupo/grupo.model.ts
@@ -0,0 +1,7 @@
+export class Grupo{
+  id! : number
+  nombre! : string
+  docente_id? : number
+  created_at! : string
+  updated_at! : string
+}
--- src/app/services/grupo-service/grupo-service.ts
+++ src/app/services/grupo-service/grupo-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
+import { Grupo } from '../../models/grupo/grupo.model';
 import { TokenJwt } from '../token';
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,13 @@ export class GrupoService {
   private apiUrl = environment.API_URL
 
 
+  getGrupos(){
+    return this.http.get<Grupo[]>(`${this.apiUrl}/grupos`,{headers : this.token.getAuthHeaders()})
+  }
 
-  
+  // este metodo regresara todos los grupos que esten signados a un docente
+  // el endpoint es /grupos
+  getGruposDocente(id : number){
+    return this.http.get<Grupo[]>(`${this.apiUrl}/grupos`,{headers:this.token.getAuthHeaders()})
+  }
 }
